dd8068bd85cdfe0ff699d8b611ba7416c10046c8
leoheisler <leoheisler@hotmail.com>
2024-11-02 13:57:42 -0300

    Termino Front tela de Cadastro

--- pages/cadastro/index.vue
+++ pages/cadastro/index.vue
@@ -1,89 +1,116 @@
 <template>
     <div class="h-screen w-screen flex flex-col justify-center items-center">
-        <div class="w-1/2 justify-center items-center h-2/3  border-black border-4 rounded-xl bg-green-700"> 
+        <div class="w-1/2 justify-center items-center h-4/5  border-black border-4 rounded-xl  bg-emerald-600"> 
             <div class="h-1/6">
-                <h1 class="w-full font-bold text-3xl flex items-center justify-center h-1/2 border-b-4 border-gray-400">CADASTRO</h1>
+                <h1 class="w-full font-bold text-3xl flex items-center justify-center h-1/2 border-b-4 border-black text-neutral-100">CADASTRO</h1>
                 <div class="flex justify-between h-1/2">
-                    <button :class="btnUser()" class="btn" @click="toUser">Usuário Comum</button>
-                    <button :class="btnProvider()" class="btn" @click="toProvider">Prestador de Serviço</button>
+                    <button :class="btnUser()" class="btn" @click="setProvider(false)">Usuário Comum</button>
+                    <button :class="btnProvider()" class="btn" @click="setProvider(true)">Prestador de Serviço</button>
                 </div>
             </div>   
             
-            <div class=" justify-between flex flex-col h-5/6 bg-slate-600">
+            <div class=" justify-between flex flex-col h-5/6 bg-emerald-600">
                 <div class="inputDiv">
                     <p class=" inputDesc">Nome Completo:*</p>
-                    <input class="inputRes w-full" type="text" placeholder=""  name="username">
+                    <input v-model="userData.nome" class="inputRes w-full" type="text" placeholder=""  name="username">
                 </div>
                 <div class="inputDiv">
                     <p class="inputDesc ">Email:*</p>
-                    <input class="inputRes w-full" type="email" placeholder=""  name="userEmail">
+                    <input v-model="userData.email" class="inputRes w-full" type="email" placeholder=""  name="userEmail">
                 </div>
                 <div class="inputDiv">
-                    <p class=" inputDesc">Cidade:*</p>
-                    <input class="inputRes w-full" type="text" placeholder=""  name="userCity">
+                    <p class="inputDesc ">CPF:*</p>
+                    <input v-model="userData.CPF" class="inputRes w-full" type="email" placeholder=""  name="userEmail">
                 </div>
                 <div class="inputDiv">
                     <p class="inputDesc">Data Nascimento:*</p>
-                    <input class="inputRes w-full" type="text" placeholder=''  name="userDate">
+                    <input v-model="userData.date" class="inputRes w-full" type="text" placeholder=''  name="userDate">
                 </div>
                 <div class="inputDiv" v-if="isProvider">
                     <p class=" inputDesc ">Localização:*</p>
-                    <input class="inputRes w-full" type="text" placeholder=""  name="userCity">
+                    <input v-model="userData.local" class="inputRes w-full" type="text" placeholder=""  name="userCity">
+                </div>
+                <div class="inputDiv" v-if="isProvider">
+                    <p class=" inputDesc ">Encanador/Eletricista:*</p>
+                    <input v-model="userData.prof" class="inputRes w-full" type="text" placeholder=""  name="userCity">
                 </div>
                 <div class="inputDiv">
                     <p class="inputDesc ">Senha:*</p>
-                    <input class="inputRes w-full" type="password" placeholder=""  name="userCity">
+                    <input v-model="userData.password" class="inputRes w-full" type="password" placeholder=""  name="userCity">
                 </div>
                 <div class="inputDiv">
                     <p class="inputDesc">Confirma Senha:*</p>
-                    <input class="inputRes w-full" type="password" placeholder=""  name="userCity">
+                    <input v-model="userData.sndPassword" class="inputRes w-full" type="password" placeholder=""  name="userCity">
                 </div>
             </div>
         </div>
 
-        <button>ENVIAR</button>
+        <div class="w-fit">
+            <NuxtLink class="btnFinal" to="/">VOLTAR</NuxtLink>
+            <button class="btnFinal" @click="teste()">ENVIAR</button>
+        </div>
+
 
     </div>
 
 </template>
 
 <script setup>
+    //DADOS
     const isProvider = ref(false)
-
-    const toUser = () =>{
-        isProvider.value = false;
+    const userData = ref( {
+        isProvider: isProvider, 
+        nome: '',
+        email: '',
+        date: '',
+        CPF: '',
+        prof: null,
+        local: null,
+        password: '',
+        sndPassword:'',
+    })
+    
+    //METODOS
+    const setProvider= (bool) =>{
+        isProvider.value = bool;
+    }
+    const teste = () =>{
+        console.log(userData)
     }
 
-    const toProvider = () =>{
-        isProvider.value = true;
-    }
-
+    //CONTROLADORES DE RENDERIZAÇÃO
     const btnUser = () => {
         return{
-            'border-white':!isProvider.value,
-            'border-black':isProvider.value,
+            'border-black':!isProvider.value,
+            'border-netral-100':isProvider.value,
+            'border-r-4': !isProvider.value
         };
     };
 
     const btnProvider = () =>{
         return{            
-            'border-white':isProvider.value,
-            'border-black':!isProvider.value,
+            'border-black':isProvider.value,
+            'border-neutral-100':!isProvider.value,
+            'border-l-4':isProvider.value
         };
     }
-   
+    
+
 </script>
 <style scoped>
 .btn{
-    @apply w-1/2 h-full border-b-4 font-bold
+    @apply w-1/2 h-full border-b-4 font-bold text-neutral-100 hover:bg-emerald-800
 }
 .inputDiv{
-    @apply flex-1
+    @apply flex-1 flex flex-col justify-center items-center 
 }
-.inpuDesc{
-    @apply h-1/3
+.inputDesc{
+    @apply h-1/3 w-2/3 font-semibold text-neutral-100
 }
 .inputRes{
-    @apply h-2/3
+    @apply h-max w-2/3
+}
+.btnFinal{
+    @apply px-12 py-4 my-0 mx-4 border-l-4 border-r-4 border-b-4 border-black rounded-b-md  hover:bg-emerald-800 bg-emerald-600 text-neutral-100 font-semibold
 }
 </style>
